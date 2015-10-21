a66bcbe744d134ba58cea49d1b2c3034b45822cd
Sam De Creus <sam.de.creus@student.howest.be>
2015-10-21 13:44:26 +0200

    added file to last commit

---
+++ VTP2015/VTP2015/Modules/Counselor/ViewModels/FileOverviewViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace VTP2015.Modules.Counselor.ViewModels
+{
+    public class FileOverviewViewModel
+    {
+        public string FileId { get; set; }
+        public string StudentFirstName { get; set; }
+        public string StudentName { get; set; }
+        public int PercentageCompleted { get; set; }
+        public string Route { get; set; }
+        public string AcademicYear { get; set; }
+    }
+}
